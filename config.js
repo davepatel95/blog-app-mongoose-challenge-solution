808fe9b6961bedb2c8529920dc0cdc293f9a28bd
Dave Patel <davepatel95@gmail.com>
2019-04-29 11:08:38 -0700

    added TEST_DATABASE_URL route to config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blog-app-mongoose-challenge'
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app-mongoose-challenge';
 exports.PORT = process.env.PORT || 8080;
