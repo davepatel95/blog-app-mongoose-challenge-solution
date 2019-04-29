5a32e5ed93ce5ff14c1778462d75e31567bec173
Dave Patel <davepatel95@gmail.com>
2019-04-29 11:07:17 -0700

    added initial populating functions

--- test/test-blog-post.js
+++ test/test-blog-post.js
@@ -12,3 +12,56 @@ const {app, runServer, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('..config');
 
 chai.use(chaiHttp);
+
+//populates db with generated data
+function seedBlogpostData() {
+    console.info('seeding blogpost data');
+    const seedData = [];
+
+    for(let i=1; i<=10; i++) {
+        seedData.push(generateBlogpostData());
+    }
+
+    return BlogPost.insertMany(seedData);
+}
+
+//returns generated blogpost data using faker.js library
+function generateBlogpostData() {
+    return {
+        author: {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName()
+        },
+        title: faker.lorem.sentence(),
+        content: faker.lorem.paragraph()
+    }
+}
+
+//deletes database
+function tearDownDb() {
+    console.warn('Deleting database');
+    return mongoose.connection.dropDatabase();
+}
+
+describe('Blogpost API resource', function() {
+    
+    before(function () {
+        return runServer(TEST_DATABASE_URL);
+    });
+
+    beforeEach(function() {
+        return seedBlogpostData();
+    });
+
+    afterEach(function() {
+        return tearDownDb();
+    });
+
+    after(function() {
+        return closeServer();
+    });
+
+    describe('GET endpoint', function() {
+
+    });
+});
