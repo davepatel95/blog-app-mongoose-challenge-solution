0161d5b090578b36c54a7de90c12fdb2ad03e0d7
Dave Patel <davepatel95@gmail.com>
2019-04-30 15:48:22 -0700

    updated config

--- test/test-blog-post.js
+++ test/test-blog-post.js
@@ -70,12 +70,122 @@ describe('Blogpost API resource', function() {
                 .then(function(_res) {
                     res = _res;
                     expect(res).to.have.status(200);
-                    console.log(res.body.posts)
                     expect(res.body).to.have.lengthOf.at.least(1);
                     return BlogPost.count();
                 })
                 .then(function(count) {
                     expect(res.body).to.have.lengthOf(count);
+                });
+        });
+
+        it('should return posts with the right fields', function() {
+            let resPost;
+            return chai.request(app)
+                .get('/posts')
+                .then(function(res) {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.a('array');
+                    expect(res.body).to.have.lengthOf.at.least(1);
+
+                    res.body.forEach(function(post) {
+                        expect(post).to.be.a('object');
+                        expect(post).to.include.keys('id', 'author', 'title', 'content');
+                    });
+                    resPost = res.body[0];
+                    return BlogPost.findById(resPost.id);
+                })
+                .then(function(post) {
+                    expect(resPost.id).to.equal(post.id);
+                    expect(resPost.title).to.equal(post.title);
+                    expect(resPost.author).to.equal(post.authorName);
+                    expect(resPost.content).to.equal(post.content);
+                });
+        });
+    });
+
+    describe('POST endpoint', function() {
+        it('should add new blogpost', function() {
+            const newPost = {
+                title: faker.lorem.sentence(),
+                author: {
+                    firstName: faker.name.firstName(),
+                    lastName: faker.name.lastName()
+                },
+                content: faker.lorem.paragraph()
+            };
+
+            return chai.request(app)
+                .post('/posts')
+                .send(newPost)
+                .then(function(res) {
+                    expect(res).to.have.status(201);
+                    expect(res).to.be.json;
+                    expect(res.body).to.be.a('object');
+                    expect(res.body).to.include.keys('id', 'content', 'author', 'title');
+                    expect(res.body.title).to.equal(newPost.title);
+                    expect(res.body.author).to.equal(`${newPost.author.firstName} ${newPost.author.lastName}`);
+                    expect(res.body.content).to.equal(newPost.content);
+                    return BlogPost.findById(res.body.id);
+                })
+                .then(function(post) {
+                    expect(post.title).to.equal(newPost.title);
+                    expect(post.content).to.equal(newPost.content);
+                    expect(post.author.firstName).to.equal(newPost.author.firstName);
+                    expect(post.author.lastName).to.equal(newPost.author.lastName);
+                });
+        });
+    });
+
+    describe('PUT endpoint', function() {
+        it('should update fields sent over', function() {
+            const updateData = {
+                title: 'The Blazers will never beat the Nuggets if McCollum does not step up',
+                author: {
+                    firstName: 'Damian',
+                    lastName: 'Lillard'
+                },
+                content: 'Damian Lillard managed to drop 39 points against the Nuggets on Monday night, yet the Blazers still wound up losing. Why? The simple answer is CJ McCollum. As long as McCollum continues to remain in this shooting slump, the Blazers will continue to get dismantled by the Jokic-led Nuggets.'
+            };
+
+            return BlogPost
+                .findOne()
+                .then(post => {
+                    updateData.id = post.id;
+
+                    return chai.request(app)
+                        .put(`/posts/${post.id}`)
+                        .send(updateData);
+                })
+                .then(res => {
+                    expect(res).to.have.status(204);
+                    return BlogPost.findById(updateData.id);
+                })
+                .then(post => {
+                    expect(post.title).to.equal(updateData.title);
+                    expect(post.content).to.equal(updateData.content);
+                    expect(post.author.firstName).to.equal(updateData.author.firstName);
+                    expect(post.author.lastName).to.equal(updateData.author.lastName);
+                });
+        });
+    });
+
+    describe('DELETE endpoint', function() {
+        it('should delete a blogpost by id', function() {
+
+            let post;
+            return BlogPost
+                .findOne()
+                .then(_post => {
+                    post = _post;
+                    return chai.request(app).delete(`/posts/${post.id}`);
+                })
+                .then(function(res) {
+                    expect(res).to.have.status(204);
+                    return BlogPost.findById(post.id);
+                })
+                .then(function(_post) {
+                    expect(_post).to.be.null;
                 });
         });
     });
